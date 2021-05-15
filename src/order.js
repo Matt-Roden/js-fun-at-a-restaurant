38e7d1fca080b5d2fc0eefb76a0cf83d92287760
Matt Roden <mattroden92@gmail.com>
2021-05-15 15:55:23 -0600

    Pass all tests for takeOrder

--- src/order.js
+++ src/order.js
@@ -1,6 +1,24 @@
 
+function takeOrder(order, orderList) {
+  if (orderList.length < 3) {
+    orderList.push(order);
+    return orderList;
+   } //else {
+  //   // if (orderList.length > 3);
+  //   // return orderList;
+  // }
+  // // orderList.push(order);
+}
+
+
+
+
+
+
+
+
 module.exports = {
-  // takeOrder,
+  takeOrder,
   // refundOrder,
   // listItems,
   // searchOrder
