d1eb9cf500e726e47c2f5ac011dacaca1a3283ad
Matt Roden <mattroden92@gmail.com>
2021-05-14 18:18:56 -0600

    Pass all tests for createMenuItem, addIngredients, formatPrice, decreasePrice

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,36 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+return(`Delicious ${name}`);
+}
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    price: price,
+    type: type,
+    name: name,
+  }
+  return menuItem;
+}
+
+//var ingredients = [];
+
+function addIngredients(newIngredient, ingredients) {
+
+  // var allIngredients = ingredients.push(newIngredient);
+  if (!ingredients.includes(newIngredient)) {
+    return ingredients.push(newIngredient);
+  }
+}
+
+function formatPrice(price) {
+  return "$" + price ;
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
   // createRecipe
 }
-
-
