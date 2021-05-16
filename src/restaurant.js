3a61b518d20e2f2973abfd105639aea07167c859
Matt Roden <mattroden92@gmail.com>
2021-05-16 14:06:25 -0600

    Pass all addMenuItem tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -10,17 +10,35 @@ function createRestaurant(restaurant) {
 }
 
 function addMenuItem (restaurantName, newItemName) {
-  return restaurantName.menus.lunch.push(newItemName);
-  for (var i = 0; i >= 1; i++) {
-    return restaurantName.menus.lunch.push(newItemName);
-  } if (newItemName.type === "lunch") {
-    return restaurantName.menus.lunch.push(newItemName);
-  } else if (newItemName.type === "breakfast") {
-    return restaurantName.menus.breakfast.push(newItemName);
-  } else if (newItemName.type === "dinner") {
-    return restaurantName.menus.dinner.push(newItemName);
+  if (newItemName.type === "lunch" && !restaurantName.menus.lunch.includes(newItemName)) {
+    restaurantName.menus.lunch.push(newItemName);
+  } else if (newItemName.type === "breakfast" && !restaurantName.menus.breakfast.includes(newItemName)) {
+    restaurantName.menus.breakfast.push(newItemName);
+  } else if (newItemName.type === "dinner" && !restaurantName.menus.dinner.includes(newItemName)) {
+    restaurantName.menus.dinner.push(newItemName);
   }
 }
+
+
+
+// {
+//   //return restaurantName.menus.lunch.push(newItemName);
+//
+//     restaurantName.menus.lunch.push(newItemName);
+//   } if (newItemName.type === "lunch") {
+//     restaurantName.menus.lunch.push(newItemName);
+//   } else if (newItemName.type === "breakfast") {
+//     return restaurantName.menus.breakfast.push(newItemName);
+//   } else if (newItemName.type === "dinner") {
+//     return restaurantName.menus.dinner.push(newItemName);
+//   } else if (restaurantName.menus.breakfast.includes(newItemName)) {
+//     return !restaurantName.menus.breakfast.push(newItemName);
+//   } else if (restaurantName.menus.lunch.includes(newItemName)) {
+//     return !restaurantName.menus.lunch.push(newItemName);
+//   } else if (restaurantName.menus.dinner.includes(newItemName)) {
+//     return !restaurantName.menus.dinner.push(newItemName);
+//   }
+// }
 
 module.exports = {
   createRestaurant,
