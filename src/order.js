340af4b266cede7be08e2810c94d1e7cb6a431c2
Matt Roden <mattroden92@gmail.com>
2021-05-16 12:08:32 -0600

    Pass some tests for order.js and restaurant.js

--- src/order.js
+++ src/order.js
@@ -1,16 +1,25 @@
 
-function takeOrder(order, orderList) {
-  if (orderList.length < 3) {
-    orderList.push(order);
-    return orderList;
-   } //else {
-  //   // if (orderList.length > 3);
-  //   // return orderList;
-  // }
-  // // orderList.push(order);
+function takeOrder(order, orderType) {
+  if (orderType.length < 3) {
+    orderType.push(order);
+    return orderType;
+   }
 }
 
+function refundOrder(orderNumber, deliveryOrders) {
+  for ( var i = 0; i <= deliveryOrders.length; ++i) {
+    if (deliveryOrders[i].orderNumber === orderNumber ) {
+      return deliveryOrders.splice(i,1);
+    }
+  }
+}
 
+//missing method, .join, and missing a variable to hold the method value
+function listItems(deliveryOrders) {
+  for ( var i = 0; i <= deliveryOrders.length; ++i) {
+    return deliveryOrders[i].item;
+  }
+}
 
 
 
@@ -19,7 +28,7 @@ function takeOrder(order, orderList) {
 
 module.exports = {
   takeOrder,
-  // refundOrder,
-  // listItems,
+  refundOrder,
+  listItems,
   // searchOrder
 }
--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,29 @@
+function createRestaurant(restaurant) {
+  return {
+    name: restaurant,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+}
 
+function addMenuItem (restaurantName, newItemName) {
+  return restaurantName.menus.lunch.push(newItemName);
+  for (var i = 0; i >= 1; i++) {
+    return restaurantName.menus.lunch.push(newItemName);
+  } if (newItemName.type === "lunch") {
+    return restaurantName.menus.lunch.push(newItemName);
+  } else if (newItemName.type === "breakfast") {
+    return restaurantName.menus.breakfast.push(newItemName);
+  } else if (newItemName.type === "dinner") {
+    return restaurantName.menus.dinner.push(newItemName);
+  }
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
