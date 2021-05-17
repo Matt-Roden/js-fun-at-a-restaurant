b64e07342f05e8bc3888dffbfce03144975c350c
Matt Roden <mattroden92@gmail.com>
2021-05-16 19:54:09 -0600

    Pass all searchOrder tests, finish project

--- src/order.js
+++ src/order.js
@@ -7,7 +7,7 @@ function takeOrder(order, orderType) {
 }
 
 function refundOrder(orderNumber, deliveryOrders) {
-  for ( var i = 0; i <= deliveryOrders.length; ++i) {
+  for ( var i = 0; i <= deliveryOrders.length; i++) {
     if (deliveryOrders[i].orderNumber === orderNumber ) {
       return deliveryOrders.splice(i,1);
     }
@@ -16,19 +16,29 @@ function refundOrder(orderNumber, deliveryOrders) {
 
 //missing method, .join, and missing a variable to hold the method value
 function listItems(deliveryOrders) {
-  for ( var i = 0; i <= deliveryOrders.length; ++i) {
-    return deliveryOrders[i].item;
+  var newInfo = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    newInfo.push(deliveryOrders[i].item);
+    // console.log(newInfo);
   }
+  return newInfo.join(', ')
 }
 
-
-
-
-
+function searchOrder(deliveryOrders, itemName) {
+  var moreNewNew = [];
+  for ( var i = 0; i < deliveryOrders.length; i++) {
+    moreNewNew.push(deliveryOrders[i].item);
+  }
+  if (moreNewNew.includes(itemName)) {
+    return true;
+  } else if (!moreNewNew.includes(itemName)) {
+    return false;
+  }
+}
 
 module.exports = {
   takeOrder,
   refundOrder,
   listItems,
-  // searchOrder
+  searchOrder
 }
